cf5f74af662fa99a358bed72ad4bf869944166ab
Shubham <samaroraytb@gmail.com>
2023-03-22 10:11:29 +0000

    Product Detail page

---
+++ src/components/ProductItemDetails/index.js
@@ -0,0 +1,196 @@
+import {Component} from 'react'
+import {Link} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import Loader from 'react-loader-spinner'
+import {BsPlusSquare, BsDashSquare} from 'react-icons/bs'
+import Header from '../Header'
+import SimilarProductItem from '../SimilarProductItem'
+import './index.css'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
+class ProductItemDetails extends Component {
+  state = {
+    productData: {},
+    similarProductsData: [],
+    dataFetchingState: apiStatusConstants.initial,
+    quantity: 1,
+  }
+
+  componentDidMount() {
+    this.getProductsDetails()
+  }
+
+  getFormattedData = data => ({
+    availability: data.availability,
+    brand: data.brand,
+    description: data.description,
+    id: data.id,
+    imageUrl: data.image_url,
+    price: data.price,
+    rating: data.rating,
+    title: data.title,
+    totalReviews: data.total_reviews,
+  })
+
+  getProductsDetails = async () => {
+    this.setState({dataFetchingState: apiStatusConstants.inProgress})
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+
+    const jwtToken = Cookies.get('jwt_token')
+
+    const apiUrl = `https://apis.ccbp.in/products/${id}`
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(apiUrl, options)
+    if (response.ok === true) {
+      const fetchedData = await response.json()
+      const updateDetailsData = this.getFormattedData(fetchedData)
+
+      const updatedSimilarProductsData = fetchedData.similar_products.map(
+        eachSimilarProduct => this.getFormattedData(eachSimilarProduct),
+      )
+
+      this.setState({
+        productData: updateDetailsData,
+        similarProductsData: updatedSimilarProductsData,
+        dataFetchingState: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({dataFetchingState: apiStatusConstants.failure})
+    }
+  }
+
+  renderSuccessProductDetail = () => {
+    const {productData, quantity, similarProductsData} = this.state
+    const {
+      availability,
+      brand,
+      description,
+      imageUrl,
+      price,
+      rating,
+      title,
+      totalReviews,
+    } = productData
+    return (
+      <div className="product-details-success-view">
+        <div className="product-details-container">
+          <img src={imageUrl} alt="product" className="product-image" />
+          <div className="product">
+            <h1 className="product-name">{title}</h1>
+            <p className="price-details">Rs {price}/-</p>
+            <div className="rating-and-reviews-count">
+              <div className="rating-container">
+                <p className="rating">{rating}</p>
+                <img
+                  src="https://assets.ccbp.in/frontend/react-js/star-img.png"
+                  alt="star"
+                  className="star"
+                />
+              </div>
+              <p className="reviews-count">{totalReviews} Reviews</p>
+            </div>
+            <p className="product-description">{description}</p>
+            <div className="label-value-container">
+              <p className="label">Available:</p>
+              <p className="value">{availability}</p>
+            </div>
+            <div className="label-value-container">
+              <p className="label">Brand:</p>
+              <p className="value">{brand}</p>
+            </div>
+            <hr className="horizontal-line" />
+            <div className="quantity-container">
+              <button
+                type="button"
+                className="quantity-controller-button"
+                onClick={this.onDecrementQuantity}
+              >
+                <BsDashSquare className="quantity-controller-icon" />
+              </button>
+              <p className="quantity">{quantity}</p>
+              <button
+                type="button"
+                className="quantity-controller-button"
+                onClick={this.onIncrementQuantity}
+              >
+                <BsPlusSquare className="quantity-controller-icon" />
+              </button>
+            </div>
+            <button type="button" className="button add-to-cart-btn">
+              ADD TO CART
+            </button>
+          </div>
+        </div>
+        <h1 className="similar-products-heading">Similar Products</h1>
+        <ul className="similar-products-list">
+          {similarProductsData.map(eachSimilarProduct => (
+            <SimilarProductItem
+              productDetails={eachSimilarProduct}
+              key={eachSimilarProduct.id}
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  renderingLoader = () => (
+    <div>
+      <Loader type="ThreeDots" color="#0b69ff" height="50" width="50" />
+    </div>
+  )
+
+  renderFetchingFailed = () => (
+    <div className="product-details-error-view-container">
+      <img
+        alt="error view"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-error-view-img.png"
+        className="error-view-image"
+      />
+      <h1 className="product-not-found-heading">Product Not Found</h1>
+      <Link to="/products">
+        <button type="button" className="button">
+          Continue Shopping
+        </button>
+      </Link>
+    </div>
+  )
+
+  renderView = () => {
+    const {dataFetchingState} = this.state
+
+    switch (dataFetchingState) {
+      case 'IN_PROGRESS':
+        return this.renderingLoader()
+      case 'SUCCESS':
+        return this.renderSuccessProductDetail()
+      default:
+        return this.renderFetchingFailed()
+    }
+  }
+
+  render() {
+    return (
+      <div className="complete-product-detail-container">
+        <Header />
+        <div className="product-item-details-container">
+          {this.renderView()}
+        </div>
+      </div>
+    )
+  }
+}
+export default ProductItemDetails
